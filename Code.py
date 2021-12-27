f0f7e7a059c12fbf38768beab942cdc2d758a802
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-27 21:53:45 +0300

    modification scripts tuple +check permissions

--- Code.py
+++ Code.py
@@ -1,5 +1,4 @@
 import os
-
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.audio.io.AudioFileClip import AudioFileClip
@@ -7,51 +6,31 @@ from pymediainfo import MediaInfo
 import random
 from pydub import AudioSegment
 
-def file_dir(url):
-    content = os.listdir(url)
-    return(content)
-
-def media_info():
-    media_info = MediaInfo.parse('C:/Users/Denis/Desktop/pythonvideo/my_concatenation.mp4')
-    for track in media_info.tracks:
-        #for k in track.to_data().keys():
-        #    print("{}.{}={}".format(track.track_type,k,track.to_data()[k]))
-        if track.track_type == 'Video':
-            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            print("{} width                 {}".format(track.track_type,track.to_data()["width"]))
-            print("{} height                {}".format(track.track_type,track.to_data()["height"]))
-            print("{} duration              {}s".format(track.track_type,track.to_data()["duration"]/1000.0))
-            print("{} duration              {}".format(track.track_type,track.to_data()["other_duration"][3][0:8]))
-            print("{} other_format          {}".format(track.track_type,track.to_data()["other_format"][0]))
-            print("{} codec_id              {}".format(track.track_type,track.to_data()["codec_id"]))
-            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        elif track.track_type == 'Audio':
-            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            print("{} format                {}".format(track.track_type,track.to_data()["format"]))
-            print("{} codec_id              {}".format(track.track_type,track.to_data()["codec_id"]))
-            print("{} channel_s             {}".format(track.track_type,track.to_data()["channel_s"]))
-            print("{} other_channel_s       {}".format(track.track_type,track.to_data()["other_channel_s"][0]))
-            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        print("********************************************************************")
-
-def media_video(img, url):
-    img = random.sample(img, len(img))
+def media_video(url):
+    list_video = tuple(os.listdir(url))
+    list_video = tuple(random.sample(list_video, len(list_video)))
     list_time = {}
-    for i in img:
+    for i in list_video:
         media_info = MediaInfo.parse(url + r'/' + i)
         for track in media_info.tracks:
             if track.track_type == 'Video':
-                list_time[i] = float("{}".format(track.to_data()["duration"]/1000.0))
+                list_time[i] = (float("{}".format(track.to_data()["duration"]/1000.0)), 
+                                    "{} x {}".format(track.to_data()["width"], track.to_data()["height"]))
     return list_time
 
 def merge_video(list_time, url, save_dir):
     check_time = 0
     list_sum = []
+    px = '640 x 360'
     for i in list_time.items():
         if check_time < 3000:
             clip = VideoFileClip(url + r'/' + i[0])
-            list_sum.append(clip)
-            check_time += i[1]
+            if i[1][1] == px:
+                list_sum.append(clip)
+                check_time += i[1][0]
+                print(i[0])
+            else:
+                print('Данный видео-файл пропущен так как он не совпадает с выбранным расширением!: ', i[0])
         else:
             print(i)
             break
@@ -84,10 +63,10 @@ url_music = input('Укажите папку с аудиофрагментами
 # r"C:\\Users\Denis\Desktop\new_music"
 save_dir = input('Укажите папку для сохранения видео (Пример: D:\\Video): ')
 # Result
-
+#url_video = "C:\\Users\Denis\Desktop\pythonvideo\image1"
+#save_dir = "F:\\"
 # merge video
-video = file_dir(url_video)
-list_video = media_video(video, url_video)
+list_video = media_video(url_video)
 merge_video(list_video, url_video, save_dir)
 
 # merge music
